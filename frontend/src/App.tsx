0b1ef66baa107df9f9cd76e93c5252caebc51b31
ChauQuangkiet <qkjet1311@gmail.com>
2025-10-23 15:27:15 +0700

    UI Design Completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
-import "./App.css";
+import { Route, Routes } from 'react-router'
+import Home from './pages/home/Home'
+import Login from './pages/auth/Login'
+import SignUp from './pages/auth/SignUp'
+import Sidebar from './components/common/Sidebar'
+import RightPanel from './components/common/RightPanel'
+import Notification from './pages/notification/Notification'
+import Profile from './pages/profile/Profile'
 
 function App() {
-  return <>Hello</>;
+  return (
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/notifications' element={<Notification />} />
+        <Route path='/profile/:username' element={<Profile />} />
+      </Routes>
+      <RightPanel />
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,6 @@
+const LoadingSpinner = ({ size = 'md' }: { size: 'sm' | 'md' | 'lg' }) => {
+  const sizeClass = `loading-${size}`
+
+  return <span className={`loading loading-spinner ${sizeClass}`} />
+}
+export default LoadingSpinner
